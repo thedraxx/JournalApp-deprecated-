562a9e8ad2ee3eb580a9eb9826464ae1e1b48bc3
Frank <55867157+thedraxx@users.noreply.github.com>
2022-06-09 13:52:06 -0300

    mantain login of the auth when you refresh

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux'
 import { Routes, Route, BrowserRouter } from 'react-router-dom'
 import { JournalScreen } from '../components/journal/JournalScreen'
-import { AuthRouter } from './AuthRouter'
+import { AuthRouter } from './AuthRouter';
+import { firebase } from '../firebase/firebase-config';
+import { login } from '../actions/auth';
 
 export const AppRouter = () => {
+    const dispatch = useDispatch();
+
+    useEffect(() => {
+
+        firebase.auth().onAuthStateChanged((user) => {
+            if (user?.uid) {
+                dispatch(login(user.uid, user.displayName))
+            }
+        });
+
+    }, [dispatch])
+
     return (
         <div>
             <BrowserRouter>
